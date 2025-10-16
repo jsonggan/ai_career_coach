fbab23ad33a724a891668f38deb3885dd0b00a4d
yibin <ysia0005@student.monash.edu>
2025-10-16 18:09:04 +0800

    Added new column "is_added_by_user" to certificates table

--- prisma/migrations/20251015095905_add_link/migration.sql
+++ prisma/migrations/20251015095905_add_link/migration.sql
@@ -1,2 +1,4 @@
 -- AlterTable
 ALTER TABLE "public"."courses" ADD COLUMN     "course_link" TEXT;
+-- AlterTable
+ALTER TABLE "public"."certificates" ADD COLUMN     "is_added_by_user" BOOLEAN NOT NULL DEFAULT FALSE;
