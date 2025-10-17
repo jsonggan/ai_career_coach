f776b1dcdd78d4f5f95fc488e4d174474699d295
yibin <ysia0005@student.monash.edu>
2025-10-17 22:43:22 +0800

    Removed manual added column "is_added_by_user"

--- prisma/migrations/20251015095905_add_link/migration.sql
+++ prisma/migrations/20251015095905_add_link/migration.sql
@@ -1,4 +1,2 @@
 -- AlterTable
 ALTER TABLE "public"."courses" ADD COLUMN     "course_link" TEXT;
--- AlterTable
-ALTER TABLE "public"."certificates" ADD COLUMN     "is_added_by_user" BOOLEAN NOT NULL DEFAULT FALSE;
